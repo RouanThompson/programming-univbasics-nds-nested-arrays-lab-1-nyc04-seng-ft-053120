f9347a685427b43478fb5d7958953eb0382c6de6
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-10 05:03:55 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,7 @@ def sorted_matrix
     ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
-  matrix.each{ |array|
+  matrix.each { |array|
     array.sort!
   }
   matrix
